50b012da8d9280e887c7a82278ea02d28e26e3c4
Péter Farkas <peterfarkas.sommelier@gmail.com>
2023-12-20 18:22:58 +0100

    init

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css'],
+})
+export class RegistrationComponent implements OnInit {
+  userAddForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    address: new FormControl(''),
+    country: new FormControl(''),
+    subscription: new FormControl(true),
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submit() {
+    console.log(this.userAddForm);
+  }
+}
